5e556cf7b6e2a2d785d098f7573f40c2e70f0cf6
Akihiro <akihiro_iwasaki@my.ofs.edu.sg>
2024-09-25 20:16:40 +0800

    changes

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I am aki");
+        System.out.println("I am akihiro");
     }
 }
