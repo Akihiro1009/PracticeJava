180791b9570857c1a9a0dcc930aebd9e01d84b1b
Upc053 <upco5364@gmail.com>
2024-09-25 12:13:39 +0800

    main

---
+++ src/Aggregation2/Display2.java
@@ -0,0 +1,17 @@
+package Aggregation2;
+
+/*public class Display2 extends Employ2 {
+    public Display2(int id2, String name, int age, Address2 address2) {
+        super(id2, name, age, address2);
+
+        System.out.println(address2.getAddress2());
+        System.out.println(getEmploy());
+    }
+
+    /*
+    public void display2(){
+        System.out.println(address2.getAddress2());
+        System.out.println(getEmploy());
+    }
+     */
+//}
