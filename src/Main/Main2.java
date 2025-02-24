aefdca52e27b6eeae48e665659f5c5275869710b
Akihiro <akihiro_iwasaki@my.ofs.edu.sg>
2025-02-24 12:27:00 +0800

    testing psudocode

--- src/Main/Main2.java
+++ src/Main/Main2.java
@@ -2,5 +2,14 @@ package Main;
 
 public class Main2 {
     public static void main(String[] args) {
+        int n = 1216;
+        int x = 0;
+
+        while (0 < n) {
+            x = x + n % 10;
+            n = n / 10;
+            System.out.println(x);
+        }
+        System.out.println(x);
     }
 }
