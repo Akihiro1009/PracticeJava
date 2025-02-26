529c6cae603dd265a4bfea3d557ffbcc1c43e41c
Akihiro <akihiro_iwasaki@my.ofs.edu.sg>
2025-02-26 13:45:32 +0800

    String to int

--- collection/src/Demo.java
+++ collection/src/Demo.java
@@ -2,6 +2,12 @@ import java.util.*;
 public class Demo {
     public static void main(String[] args) {
         Collection list = new Collection();
-
+        list.addItem("Ada"); System.out.println(list);
+        list.addItem("Ali"); System.out.println(list);
+        list.addItem("Bob"); System.out.println(list);
+        list.addItem("Jill"); System.out.println(list);
+        list.addItem("Kyle");
+        System.out.println("OOOO");
+        System.out.println(list);
     }
 }
