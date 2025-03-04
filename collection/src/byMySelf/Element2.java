a5bedc518a248f0d05ebdbbcd3ae37e15d7af665
Akihiro <akihiro_iwasaki@my.ofs.edu.sg>
2025-03-04 22:12:10 +0800

    new blog

--- collection/src/byMySelf/Element2.java
+++ collection/src/byMySelf/Element2.java
@@ -1,4 +1,11 @@
 package byMySelf;
 
 public class Element2 {
+    private String data;
+    Element2 next;
+
+    public Element2(String data) {
+        this.data = data;
+        this.next = null;
+    }
 }
