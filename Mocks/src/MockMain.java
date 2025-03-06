e60d1931a9c224e4604a421bf97a9d338480caf3
Akihiro <akihiro_iwasaki@my.ofs.edu.sg>
2025-03-06 13:45:57 +0800

    for mock

---
+++ Mocks/src/MockMain.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+public class MockMain {
+    public static void main(String[] args) {
+        List<String> list1 = List.of("Iwasaki ", "Akihiro", "Iwasaki", "Takatoshi", "Papinda", "Suchada", "Yamada", "Taro", "Satou", "Jyunichi", "Tanaka", "Toshizou", "Donald", "Trump"); //7 people.
+
+
+        Collection listName = new Collection();
+        for(int j = 0; j < 14; j++) {
+            listName.addItem(list1.get(j));
+            System.out.println(listName);
+        }
+        System.out.println(listName);
+
+        /*while (true){
+            int n = 0;
+            list.addItem(list1.get(n));
+            System.out.println(list);
+            n++;
+            }
+        */
+
+        //how to change collection to Array
+
+        for(int n = 0; n < 14; n++){
+
+        }
+
+
+        String[] surName = new String[7];
+        String[] firstName = new String[7];
+        boolean name = true;
+
+        for (int i = 0; i < 5; i++) {
+            if (name) {
+                System.out.println(i + ": this is true");
+                name = false;
+            } else {
+                System.out.println(i + ": this is false");
+                name = true;
+            }
+        }
+
+    }
+}
