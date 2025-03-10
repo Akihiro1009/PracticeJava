8ad23f2189792793efc97abe330a466a0373a03e
Akihiro <akihiro_iwasaki@my.ofs.edu.sg>
2025-03-11 02:02:57 +0800

    if statement

--- sort/src/selection.java
+++ sort/src/selection.java
@@ -2,9 +2,18 @@ import java.util.Random;
 import java.util.Arrays;
 
 public class selection {
-    public void selectionSort(){
-        //int[] numbers = new int[10];
-
+    public static void selectionSort(int[] num){
+        for(int i = 0; i < num.length - 1 ; i++){
+            int min = i;
+            for(int j = i + 1; j < num.length; j++){
+                if(num[j] < num[min]){
+                    min = j;
+                }
+            }
+            int temp = num[i];
+            num[i] = num[min];
+            num[min] = temp;
+        }
     }
 
     public static void main(String[] args) {
@@ -13,6 +22,9 @@ public class selection {
         for (int i = 0; i < num.length; i++) {
             num[i] = random.nextInt(100);
         }
+        System.out.println(Arrays.toString(num));
+
+        selectionSort(num);
         System.out.println(Arrays.toString(num));
     }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,5 +10,36 @@ public class Main {
 
         number = Integer.parseInt(numberString) + number;
         System.out.println(number);
+
+        ifStatement();
+    }
+
+    public static void ifStatement(){
+        int a = 10;
+
+        /*if(a == 10){
+            System.out.println("a is 10 : 1st exe");
+        }
+        if(a == 10){
+            System.out.println("a is 10 : 2nd exe");
+        }
+        if(a == 10){
+            System.out.println("a is 10 : 3rd exe");
+        }
+        if(a == 10){
+            System.out.println("a is 10 : 4th exe");
+        }*/
+
+        if(a == 10){
+            System.out.println("a is 10 : 1st exe");
+        } else if(a == 10){
+            System.out.println("a is 10 : 2nd exe");
+        } else if(a == 10){
+            System.out.println("a is 10 : 3rd exe");
+        } if(a == 10){
+            System.out.println("a is 10 : 4th exe");
+        } else if(a != 10){
+            System.out.println("a is 10 : 5th exe"); //won't be executed
+        }
     }
 }
